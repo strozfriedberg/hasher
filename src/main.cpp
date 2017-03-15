2e532dd20b9b2819f0a79992fbce2f2345b30dc4
Joel Uckelman <juckelman@strozfriedberg.co.uk>
2017-03-15 14:44:54 +0000

    Make sample hasher read from a file, not from stdin.

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,44 @@
 #include "hasher.h"
 #include "util.h"
 
+#include <fstream>
 #include <iostream>
 
+#include <boost/lexical_cast.hpp>
+
 int main(int argc, char** argv) {
+  const int algs = boost::lexical_cast<int>(argv[1]);
+
   auto hasher = make_unique_del(
-    sfhash_create_hasher(MD5 | SHA1 | SHA256),
+    sfhash_create_hasher(algs),
     sfhash_destroy_hasher
   );
 
   char buf[4096];
-  
+
+  std::ifstream f;
+  f.rdbuf()->pubsetbuf(0, 0); // unbuffered
+  f.open(argv[2], std::ios_base::in | std::ios_base::binary);
+
   do {
-    std::cin.read(buf, sizeof(buf));
-    sfhash_update_hasher(hasher.get(), buf, buf + std::cin.gcount());
-  } while (std::cin);
+    f.read(buf, sizeof(buf));
+    sfhash_update_hasher(hasher.get(), buf, buf + f.gcount());
+  } while (f);
 
   SFHASH_HashValues hashes;
   sfhash_get_hashes(hasher.get(), &hashes);
 
-  std::cout << to_hex(hashes.md5, hashes.md5+16)
-            << '\n' 
-            << to_hex(hashes.sha1, hashes.sha1+20)
-            << '\n' 
-            << to_hex(hashes.sha256, hashes.sha256+32)
-            << '\n';
+  if (algs & MD5) {
+    std::cout << to_hex(hashes.md5, hashes.md5+16) << '\n'; 
+  }
+
+  if (algs & SHA1) {
+    std::cout << to_hex(hashes.sha1, hashes.sha1+20) << '\n'; 
+  }
+
+  if (algs & SHA256) {
+    std::cout << to_hex(hashes.sha256, hashes.sha256+32) << '\n';
+  }
 
   return 0;
 }
