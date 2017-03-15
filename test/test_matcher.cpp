c7801ff17b6314887c9e9171e167a65fe9db364c
Joel Uckelman <juckelman@strozfriedberg.co.uk>
2017-03-15 16:39:09 +0000

    Added test for sfhash_matcher_has_filename.

--- test/test_matcher.cpp
+++ test/test_matcher.cpp
@@ -35,19 +35,19 @@ SCOPE_TEST(tokenizeNonempty) {
   SCOPE_ASSERT(++i == end);
 }
 
-SCOPE_TEST(iterateHashset) {
-  const char txt[] = 
+const char HSET[] = 
     "x\t123\t1eb328edc1794050fa64c6c62d6656d5c6b1b6b2\n"
     "y\t456789\t3937e80075fc5a0f219c7d68e5e171ec7fe6dee3\n"
     "filename with spaces\t0\t5e810a94c86ff057849bfa992bd176d8f743d160\n";
 
+SCOPE_TEST(iterateHashset) {
   const std::tuple<std::string, size_t, sha1_t> exp[] = {
     { "x", 123, to_bytes<20>("1eb328edc1794050fa64c6c62d6656d5c6b1b6b2") },
     { "y", 456789, to_bytes<20>("3937e80075fc5a0f219c7d68e5e171ec7fe6dee3") },
     { "filename with spaces", 0, to_bytes<20>("5e810a94c86ff057849bfa992bd176d8f743d160") }
   };
 
-  HashsetIterator i(txt, txt + std::strlen(txt));
+  HashsetIterator i(HSET, HSET + std::strlen(HSET));
   const HashsetIterator iend;  
 
   SCOPE_ASSERT(i != iend);
@@ -60,7 +60,7 @@ SCOPE_TEST(iterateHashset) {
 }
 
 SCOPE_TEST(loadHashset) {
-  const char txt[] = 
+  const char HSET[] = 
     "x\t123\t1eb328edc1794050fa64c6c62d6656d5c6b1b6b2\n"
     "y\t456789\t3937e80075fc5a0f219c7d68e5e171ec7fe6dee3\n"
     "filename with spaces\t0\t5e810a94c86ff057849bfa992bd176d8f743d160\n";
@@ -71,46 +71,52 @@ SCOPE_TEST(loadHashset) {
     { 456789, to_bytes<20>("3937e80075fc5a0f219c7d68e5e171ec7fe6dee3")}
   };
 
-  const SFHASH_FileMatcher m{load_hashset(txt, txt + std::strlen(txt))};
+  const SFHASH_FileMatcher m{load_hashset(HSET, HSET + std::strlen(HSET))};
 
   SCOPE_ASSERT_EQUAL(m.table, exp);
 }
 
 SCOPE_TEST(has_size) {
-  const char txt[] = 
+  const char HSET[] = 
     "x\t123\t1eb328edc1794050fa64c6c62d6656d5c6b1b6b2\n"
     "y\t456789\t3937e80075fc5a0f219c7d68e5e171ec7fe6dee3\n"
     "filename with spaces\t0\t5e810a94c86ff057849bfa992bd176d8f743d160\n";
 
-  auto matcher = make_unique_del(
-    sfhash_create_matcher(txt, txt + std::strlen(txt), nullptr),
+  auto m = make_unique_del(
+    sfhash_create_matcher(HSET, HSET + std::strlen(HSET), nullptr),
     sfhash_destroy_matcher
   );
 
-  SCOPE_ASSERT(!sfhash_matcher_has_size(matcher.get(), 122));
-  SCOPE_ASSERT(sfhash_matcher_has_size(matcher.get(), 123));
-  SCOPE_ASSERT(!sfhash_matcher_has_size(matcher.get(), 124));
+  SCOPE_ASSERT(!sfhash_matcher_has_size(m.get(), 122));
+  SCOPE_ASSERT(sfhash_matcher_has_size(m.get(), 123));
+  SCOPE_ASSERT(!sfhash_matcher_has_size(m.get(), 124));
 }
 
 SCOPE_TEST(has_hash) {
-  const char txt[] = 
-    "x\t123\t1eb328edc1794050fa64c6c62d6656d5c6b1b6b2\n"
-    "y\t456789\t3937e80075fc5a0f219c7d68e5e171ec7fe6dee3\n"
-    "filename with spaces\t0\t5e810a94c86ff057849bfa992bd176d8f743d160\n";
-
   const sha1_t hashes[] = {
     to_bytes<20>("5e810a94c86ff057849bfa992bd176d8f743d160"),
     to_bytes<20>("1eb328edc1794050fa64c6c62d6656d5c6b1b6b2"),
     to_bytes<20>("3937e80075fc5a0f219c7d68e5e171ec7fe6dee3")
   };
 
-  auto matcher = make_unique_del(
-    sfhash_create_matcher(txt, txt + std::strlen(txt), nullptr),
+  auto m = make_unique_del(
+    sfhash_create_matcher(HSET, HSET + std::strlen(HSET), nullptr),
     sfhash_destroy_matcher
   );
 
-  SCOPE_ASSERT(!sfhash_matcher_has_hash(matcher.get(), 122, hashes[0].data()));
-  SCOPE_ASSERT(!sfhash_matcher_has_hash(matcher.get(), 123, hashes[0].data()));
-  SCOPE_ASSERT(sfhash_matcher_has_hash(matcher.get(), 123, hashes[1].data()));
-  SCOPE_ASSERT(!sfhash_matcher_has_hash(matcher.get(), 124, hashes[0].data()));
+  SCOPE_ASSERT(!sfhash_matcher_has_hash(m.get(), 122, hashes[0].data()));
+  SCOPE_ASSERT(!sfhash_matcher_has_hash(m.get(), 123, hashes[0].data()));
+  SCOPE_ASSERT(sfhash_matcher_has_hash(m.get(), 123, hashes[1].data()));
+  SCOPE_ASSERT(!sfhash_matcher_has_hash(m.get(), 124, hashes[0].data()));
+}
+
+SCOPE_TEST(has_filename) {
+  auto m = make_unique_del(
+    sfhash_create_matcher(HSET, HSET + std::strlen(HSET), nullptr),
+    sfhash_destroy_matcher
+  );
+
+  SCOPE_ASSERT(sfhash_matcher_has_filename(m.get(), "xyzzy"));
+  SCOPE_ASSERT(sfhash_matcher_has_filename(m.get(), "123x"));
+  SCOPE_ASSERT(!sfhash_matcher_has_filename(m.get(), "filename with space"));
 }
