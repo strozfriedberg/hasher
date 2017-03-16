9d5b9bc45b72a2e3b11579adbebaa1ef86a71f49
Joel Uckelman <juckelman@strozfriedberg.co.uk>
2017-03-16 14:50:37 +0000

    Added some comments.

--- include/hasher.h
+++ include/hasher.h
@@ -20,10 +20,12 @@ typedef struct SFHASH_HashValues {
 
 struct SFHASH_Hasher;
 
+// hashAlgs a bitwise-or of desired SFHASH_HashAlgorithms
 SFHASH_Hasher* sfhash_create_hasher(uint32_t hashAlgs);
 
-SFHASH_Hasher* sfhash_clone_hasher(const SFHASH_Hasher* hasher);
+//SFHASH_Hasher* sfhash_clone_hasher(const SFHASH_Hasher* hasher);
 
+// update the hash(es) with more data
 void sfhash_update_hasher(SFHASH_Hasher* hasher, const void* beg, const void* end);
 
 void sfhash_get_hashes(SFHASH_Hasher* hasher, SFHASH_HashValues* out_hashes);
@@ -36,18 +38,29 @@ struct SFHASH_FileMatcher;
 
 struct LG_Error;
 
+// Input is a three-column tab-separated UTF-8 text file, where each line
+// is of the form:
+//  
+//  Filename\tFilesize\tSHA1\n
+//
 SFHASH_FileMatcher* sfhash_create_matcher(const char* beg, const char* end, LG_Error** err);
 
+// returns nonzero if the given file size occurs in the hash set
 int sfhash_matcher_has_size(const SFHASH_FileMatcher* matcher, uint64_t size);
 
+// returns nonzero if the given file size and hash occur in the hash set
 int sfhash_matcher_has_hash(const SFHASH_FileMatcher* matcher, uint64_t size, const uint8_t* sha1);
 
+// returns nonzero if the given filename matches the hash set
 int sfhash_matcher_has_filename(const SFHASH_FileMatcher* matcher, const char* filename);
 
+// returns the size in bytes of the buffer for sfhash_write_binary_matcher
 int sfhash_matcher_size(const SFHASH_FileMatcher* matcher);
 
+// serializes the matcher to a buffer
 void sfhash_write_binary_matcher(const SFHASH_FileMatcher* matcher, void* buf);
 
+// unserizalizes the matcher from a buffer
 SFHASH_FileMatcher* sfhash_read_binary_matcher(void* beg, void* end);
 
 void sfhash_destroy_matcher(SFHASH_FileMatcher* matcher);
