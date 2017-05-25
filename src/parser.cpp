40842059e138e098d15c77eade98f2805e976411
Joel Uckelman <juckelman@strozfriedberg.co.uk>
2017-05-25 14:53:39 +0100

    Strip whitespace from size, hash columns in hashsets. Accept empty or missing trailing columns. Accept (and ignore) additional columns beyond the third.

--- src/parser.cpp
+++ src/parser.cpp
@@ -3,58 +3,103 @@
 
 #include <boost/lexical_cast.hpp>
 
+const char* find_nonws(const char* beg, const char* end) {
+  return std::find_if_not(beg, end, [](char c){ return c == ' '; });
+}
+
 std::tuple<uint8_t, std::string, uint64_t, sha1_t> parse_line(const char* beg, const char * const end) {
-  const char* i = beg;
-  const char* j;
 
-  if (i == end) {
-    // we have a blank line
-    return { BLANK_LINE, "", 0, sha1_t() };
+  bool have_name = false, have_size = false, have_hash = false;
+  uint8_t flags = BLANK_LINE;
+  std::string name;
+  uint64_t size = 0;
+  sha1_t hash{};
+
+  if (beg != end) {
+    const char* cbeg = beg;
+    const char* cend;
+
+    // go to column 1
+    cend = std::find(cbeg, end, '\t');
+
+    // parse the filename
+    if (cbeg != cend) {
+      // filename column is nonempty
+      name.assign(cbeg, cend);
+      have_name = true;
+    }
+
+    // go to column 2
+    cbeg = std::min(cend + 1, end);
+    cend = std::find(cbeg, end, '\t');
+
+    // parse the file size
+    if (cbeg != cend) {
+      // file size column is nonempty
+
+      // eat whitespace
+      const char* i = find_nonws(cbeg, cend);
+      if (i != cend) {
+        
+        // check for a minus sign, as lexical_cast rolls over negatives
+        THROW_IF(*i == '-', "bad file size '" << std::string(i, cend) << "'");
+
+        // eat trailing whitespace
+        const char* j = std::find(i, cend, ' ');
+        if (j != cend) {
+          // reject trailing nonwhitespace
+          THROW_IF(
+            find_nonws(j + 1, cend) != cend,
+            "bad file size '" << std::string(i, cend) << "'"
+          );
+        }
+
+        try {
+          size = boost::lexical_cast<uint64_t>(i, j - i);
+        }
+        catch (const boost::bad_lexical_cast& e) {
+          THROW("bad file size '" << std::string(i, j) << "'");
+        }
+
+        have_size = true;
+      }
+    }
+
+    // go to column 3
+    cbeg = std::min(cend + 1, end);
+    cend = std::find(cbeg, end, '\t');
+
+    // parse the hash
+    if (cbeg != cend) {
+      // eat whitespace
+      const char* i = find_nonws(cbeg, cend);
+      const char* j = std::find(std::min(i + 1, cend), cend, ' ');
+
+      THROW_IF(j - i != 40,
+              "file hash is " << (j - i > 40 ? "longer" : "shorter")
+                              << " than 40 characters");
+  
+      // reject trailing nonwhitespace
+      THROW_IF(
+        find_nonws(j + 1, cend) != cend,
+        "bad hash '" << std::string(i, cend) << "'"
+      );
+
+      hash = to_bytes<20>(i);
+      have_hash = true;
+    }
   }
 
-  // read the filename (possibly the sole column, possilbly an empty column)
-  j = std::find(i, end, '\t');
-  std::string name(i, j);
+  THROW_IF(have_hash && !have_size, "missing file size");
+  THROW_IF(have_size && !have_hash, "missing hash");
 
-  if (j == end) {
-    // we have only the filename
-    return { HAS_FILENAME, std::move(name), 0, sha1_t() };
+  if (have_name) {
+    flags |= HAS_FILENAME;
   }
 
-  // read the file size
-  i = j + 1;
-  THROW_IF(i >= end, "missing file size and hash");
-
-  // we must check for a minus sign, as lexical_cast rolls over negatives
-  const char tab_or_minus[] = "\t-";
-  j = std::find_first_of(i, end, tab_or_minus, tab_or_minus + 2);
-  THROW_IF(j == end, "missing file hash");
-  if (*j == '-') {
-    j = std::find(j, end, '\t');
-    THROW("bad file size '" << std::string(i, j) << "'");
+  if (have_size) {
+    flags |= HAS_SIZE_AND_HASH;
   }
 
-  uint64_t size;
-  try {
-    size = boost::lexical_cast<uint64_t>(i, j - i);
-  }
-  catch (const boost::bad_lexical_cast& e) {
-    THROW("bad file size '" << std::string(i, j) << "'");
-  }
-
-  // read the SHA1
-  i = j + 1;
-  THROW_IF(i >= end, "missing file hash");
-  j = i + 40;
-  THROW_IF(
-    j != end,
-    "file hash is " << (j < end ? "longer" : "shorter")
-                    << " than 40 characters"
-  );
-  sha1_t hash = to_bytes<20>(i);
-
-  return {
-    HAS_SIZE_AND_HASH | (name.empty() ? 0 : HAS_FILENAME),
-    std::move(name), size, std::move(hash)
-  };
+  return { flags, std::move(name), size, std::move(hash) };
 }
--- test/test_parser.cpp
+++ test/test_parser.cpp
@@ -104,62 +104,101 @@ SCOPE_TEST(iterateLinesCRLFNoTerminalEOL) {
   SCOPE_ASSERT(++i == end);
 }
 
-SCOPE_TEST(iterateHashset) {
+SCOPE_TEST(iterateHashset1) {
   const char HSET[] =
-    "x\t123\t1eb328edc1794050fa64c6c62d6656d5c6b1b6b2\n"
-    "y\t456789\t3937e80075fc5a0f219c7d68e5e171ec7fe6dee3\n"
+    "a\t123\t1eb328edc1794050fa64c6c62d6656d5c6b1b6b2\n"
+    "b\t456789\t3937e80075fc5a0f219c7d68e5e171ec7fe6dee3\n"
+    "c\t 456789\t3937e80075fc5a0f219c7d68e5e171ec7fe6dee3\n"
+    "d\t456789 \t3937e80075fc5a0f219c7d68e5e171ec7fe6dee3\n"
+    "e\t 456789 \t3937e80075fc5a0f219c7d68e5e171ec7fe6dee3\n"
+    "f\t456789\t 3937e80075fc5a0f219c7d68e5e171ec7fe6dee3\n"
+    "g\t456789\t3937e80075fc5a0f219c7d68e5e171ec7fe6dee3 \n"
+    "h\t456789\t 3937e80075fc5a0f219c7d68e5e171ec7fe6dee3 \n"
     "filename with spaces\t0\t5e810a94c86ff057849bfa992bd176d8f743d160\n"
     "filename_only\n"
+    "filename_only\t\n"
+    "filename_only\t\t\n"
     "\n"
-    "\t8675309\t561b0fb9acc2dbb5edaf595558a1e6112a1f24a0\n";
+    "\t\n"
+    "\t\t\n"
+    "\t8675309\t561b0fb9acc2dbb5edaf595558a1e6112a1f24a0\n"
+    "extra column!\t8675309\t561b0fb9acc2dbb5edaf595558a1e6112a1f24a0\textra column!\n";
 
   const std::tuple<uint8_t, std::string, uint64_t, sha1_t> exp[] = {
-    { HAS_FILENAME | HAS_SIZE_AND_HASH, "x", 123, to_bytes<20>("1eb328edc1794050fa64c6c62d6656d5c6b1b6b2") },
-    { HAS_FILENAME | HAS_SIZE_AND_HASH, "y", 456789, to_bytes<20>("3937e80075fc5a0f219c7d68e5e171ec7fe6dee3") },
+    { HAS_FILENAME | HAS_SIZE_AND_HASH, "a", 123, to_bytes<20>("1eb328edc1794050fa64c6c62d6656d5c6b1b6b2") },
+    { HAS_FILENAME | HAS_SIZE_AND_HASH, "b", 456789, to_bytes<20>("3937e80075fc5a0f219c7d68e5e171ec7fe6dee3") },
+    { HAS_FILENAME | HAS_SIZE_AND_HASH, "c", 456789, to_bytes<20>("3937e80075fc5a0f219c7d68e5e171ec7fe6dee3") },
+    { HAS_FILENAME | HAS_SIZE_AND_HASH, "d", 456789, to_bytes<20>("3937e80075fc5a0f219c7d68e5e171ec7fe6dee3") },
+    { HAS_FILENAME | HAS_SIZE_AND_HASH, "e", 456789, to_bytes<20>("3937e80075fc5a0f219c7d68e5e171ec7fe6dee3") },
+    { HAS_FILENAME | HAS_SIZE_AND_HASH, "f", 456789, to_bytes<20>("3937e80075fc5a0f219c7d68e5e171ec7fe6dee3") },
+    { HAS_FILENAME | HAS_SIZE_AND_HASH, "g", 456789, to_bytes<20>("3937e80075fc5a0f219c7d68e5e171ec7fe6dee3") },
+    { HAS_FILENAME | HAS_SIZE_AND_HASH, "h", 456789, to_bytes<20>("3937e80075fc5a0f219c7d68e5e171ec7fe6dee3") },
     { HAS_FILENAME | HAS_SIZE_AND_HASH, "filename with spaces", 0, to_bytes<20>("5e810a94c86ff057849bfa992bd176d8f743d160") },
     { HAS_FILENAME, "filename_only", 0, sha1_t() },
+    { HAS_FILENAME, "filename_only", 0, sha1_t() },
+    { HAS_FILENAME, "filename_only", 0, sha1_t() },
     { BLANK_LINE, "", 0, sha1_t() },
-    { HAS_SIZE_AND_HASH, "", 8675309, to_bytes<20>("561b0fb9acc2dbb5edaf595558a1e6112a1f24a0") }
+    { BLANK_LINE, "", 0, sha1_t() },
+    { BLANK_LINE, "", 0, sha1_t() },
+    { HAS_SIZE_AND_HASH, "", 8675309, to_bytes<20>("561b0fb9acc2dbb5edaf595558a1e6112a1f24a0") },
+    { HAS_FILENAME | HAS_SIZE_AND_HASH, "extra column!", 8675309, to_bytes<20>("561b0fb9acc2dbb5edaf595558a1e6112a1f24a0") }
   };
 
   LineIterator l(HSET, HSET + std::strlen(HSET));
   const LineIterator lend(HSET + std::strlen(HSET), HSET + std::strlen(HSET));
 
-  SCOPE_ASSERT(l != lend);
-  SCOPE_ASSERT_EQUAL(parse_line(l->first, l->second), exp[0]);
-  SCOPE_ASSERT(++l != lend);
-  SCOPE_ASSERT_EQUAL(parse_line(l->first, l->second), exp[1]);
-  SCOPE_ASSERT(++l != lend);
-  SCOPE_ASSERT_EQUAL(parse_line(l->first, l->second), exp[2]);
-  SCOPE_ASSERT(++l != lend);
-  SCOPE_ASSERT_EQUAL(parse_line(l->first, l->second), exp[3]);
-  SCOPE_ASSERT(++l != lend);
-  SCOPE_ASSERT_EQUAL(parse_line(l->first, l->second), exp[4]);
-  SCOPE_ASSERT(++l != lend);
-  SCOPE_ASSERT_EQUAL(parse_line(l->first, l->second), exp[5]);
-  SCOPE_ASSERT(++l == lend);
+  for (const auto& e: exp) {
+    SCOPE_ASSERT(l != lend);
+    SCOPE_ASSERT_EQUAL(parse_line(l->first, l->second), e);
+    ++l;
+  }
+
+  SCOPE_ASSERT(l == lend);
 }
 
 SCOPE_TEST(iterateHashsetBad) {
   const char HSET[] =
     "too short\t123\t1eb328edc1794050fa64c6c62d6656d5c6b1b6b\n"
     "too long\t123\t1eb328edc1794050fa64c6c62d6656d5c6b1b6bbb\n"
+    "hash with trailing juni\t123\t1eb328edc1794050fa64c6c62d6656d5c6b1b6bbb junk\n"
     "bogus hash\t123\tBOGUSBOGUSBOGUSBOGUSBOGUSBOGUSBOGUSBOGUS\n"
-    "\t\t\n"
     "missing hash\t123\t\n"
     "missing hash column\t123\n"
     "bogus size\tNAN\t1eb328edc1794050fa64c6c62d6656d5c6b1b6b2\n"
     "size with trailing junk\t123-\t1eb328edc1794050fa64c6c62d6656d5c6b1b6b2\n"
+    "size with trailing junk\t123 123\t1eb328edc1794050fa64c6c62d6656d5c6b1b6b2\n"
     "size too small\t-1\t1eb328edc1794050fa64c6c62d6656d5c6b1b6b2\n"
     "size too large\t18446744073709551617\t1eb328edc1794050fa64c6c62d6656d5c6b1b6b2\n";
 
   LineIterator l(HSET, HSET + std::strlen(HSET));
   const LineIterator lend(HSET + std::strlen(HSET), HSET + std::strlen(HSET));
 
-  for (int i = 0; i < 10; ++i, ++l) {
+  for (int i = 0; i < 11; ++i, ++l) {
     SCOPE_ASSERT(l != lend);
     SCOPE_EXPECT(parse_line(l->first, l->second), std::runtime_error);
   }
 
   SCOPE_ASSERT(++l == lend);
+}
+
+SCOPE_TEST(iterateHashset2) {
+  const char HSET[] =
+    "filename with spaces\t0\tda39a3ee5e6b4b0d3255bfef95601890afd80709\r\n"
+    "filename êèðèëëèöà\t1\t7f8fc202eb553370d4a05b446e57fef1734eca8f\r\n";
+
+  const std::tuple<uint8_t, std::string, uint64_t, sha1_t> exp[] = {
+    { HAS_FILENAME | HAS_SIZE_AND_HASH, "filename with spaces", 0, to_bytes<20>("da39a3ee5e6b4b0d3255bfef95601890afd80709") },
+    { HAS_FILENAME | HAS_SIZE_AND_HASH, "filename êèðèëëèöà", 1, to_bytes<20>("7f8fc202eb553370d4a05b446e57fef1734eca8f") }
+  };
+
+  LineIterator l(HSET, HSET + std::strlen(HSET));
+  const LineIterator lend(HSET + std::strlen(HSET), HSET + std::strlen(HSET));
+
+  for (const auto& e: exp) {
+    SCOPE_ASSERT(l != lend);
+    SCOPE_ASSERT_EQUAL(parse_line(l->first, l->second), e);
+    ++l;
+  }
+
+  SCOPE_ASSERT(l == lend);
 }
