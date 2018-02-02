b18748048aac41ebebe19e92baa23aa3b9439462
Joel Uckelman <juckelman@strozfriedberg.co.uk>
2018-02-02 15:58:32 +0000

    Don't clobber $LD_LIBRARY_PATH; it may already have stuff we need in it.

--- python/test.sh
+++ python/test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
 if [ -e src/.libs/libhasher.so ]; then
-  LD_LIBRARY_PATH=src/.libs:/usr/local/lib python/test.py -v
+  LD_LIBRARY_PATH=src/.libs:$LD_LIBRARY_PATH python/test.py -v
 fi
