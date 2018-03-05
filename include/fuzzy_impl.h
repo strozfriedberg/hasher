d1586a539e20c98d4b3bcba4aecd875e28724541
Zack Weger <ZWeger@StrozFriedberg.com>
2018-03-05 14:11:32 -0500

    Move fuzzy_impl implementations to src/

--- include/fuzzy_impl.h
+++ include/fuzzy_impl.h
@@ -7,53 +7,28 @@
 
 class FuzzyHasher: public HasherImpl {
 public:
-  virtual ~FuzzyHasher() {
-    fuzzy_free(ctx);
-  }
+  virtual ~FuzzyHasher();
 
-  FuzzyHasher():
-    ctx(fuzzy_new())
-  {}
+  FuzzyHasher();
 
-  FuzzyHasher(const FuzzyHasher& other):
-    ctx(fuzzy_clone(other.ctx))
-  {}
+  FuzzyHasher(const FuzzyHasher& other);
 
   FuzzyHasher(FuzzyHasher&&) = default;
 
   FuzzyHasher& operator=(FuzzyHasher&&) = default;
 
-  virtual void update(const uint8_t* beg, const uint8_t* end) {
-    if (!fuzzy_update(ctx, beg, end-beg)) {
-      // TODO: error!
-    }
-  }
+  virtual void update(const uint8_t* beg, const uint8_t* end);
 
-  virtual void set_total_input_length(uint64_t len) {
-    if (!fuzzy_set_total_input_length(ctx, len)) {
-      // TODO: error!
-    }
-  }
+  virtual void set_total_input_length(uint64_t len);
 
-  virtual void get(void* val) {
-    if (!fuzzy_digest(ctx, static_cast<char*>(val), 0)) {
-      // TODO: error!
-    }
-  }
+  virtual void get(void* val);
 
-  virtual void reset() {
-    fuzzy_free(ctx);
-    ctx = fuzzy_new();
-  }
+  virtual void reset();
 
-  virtual FuzzyHasher* clone() const {
-    return new FuzzyHasher(*this);
-  }
+  virtual FuzzyHasher* clone() const;
 
 private:
   fuzzy_state* ctx;
 };
 
-inline std::unique_ptr<HasherImpl> make_fuzzy_hasher() {
-  return std::unique_ptr<FuzzyHasher>(new FuzzyHasher());
-}
+std::unique_ptr<HasherImpl> make_fuzzy_hasher();
--- src/fuzzy.cpp
+++ src/fuzzy.cpp
@@ -1,6 +1,7 @@
 #include "hasher.h"
 #include "parser.h"
 #include "fuzzy_matcher.h"
+#include "fuzzy_impl.h"
 
 #include <boost/lexical_cast.hpp>
 #include <boost/archive/iterators/binary_from_base64.hpp>
@@ -278,4 +279,46 @@ std::unordered_set<uint64_t> FuzzyHash::chunks() {
 
 std::unordered_set<uint64_t> FuzzyHash::double_chunks() {
   return decode_chunks(double_block());
+}
+
+FuzzyHasher::~FuzzyHasher() {
+  fuzzy_free(ctx);
+}
+
+FuzzyHasher::FuzzyHasher() :
+  ctx(fuzzy_new())
+{}
+
+FuzzyHasher::FuzzyHasher(const FuzzyHasher& other) :
+  ctx(fuzzy_clone(other.ctx))
+{}
+
+void FuzzyHasher::update(const uint8_t* beg, const uint8_t* end) {
+  if (!fuzzy_update(ctx, beg, end-beg)) {
+    // TODO: error!
+  }
+}
+
+void FuzzyHasher::set_total_input_length(uint64_t len) {
+  if (!fuzzy_set_total_input_length(ctx, len)) {
+    // TODO: error!
+  }
+}
+
+void FuzzyHasher::get(void* val) {
+  if (!fuzzy_digest(ctx, static_cast<char*>(val), 0)) {
+    // TODO: error!
+  }
+}
+void FuzzyHasher::reset() {
+  fuzzy_free(ctx);
+  ctx = fuzzy_new();
+}
+
+FuzzyHasher* FuzzyHasher::clone() const {
+  return new FuzzyHasher(*this);
+}
+
+std::unique_ptr<HasherImpl> make_fuzzy_hasher() {
+  return std::unique_ptr<FuzzyHasher>(new FuzzyHasher());
 }
