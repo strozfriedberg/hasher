ddbc805c2a373e37cbdabbaab38cc9afeabf5eb5
Zack Weger <ZWeger@StrozFriedberg.com>
2018-03-06 11:03:24 -0500

    Split src/fuzzy.cpp into fuzzy_hasher and fuzzy_matcher

---
+++ src/fuzzy_hasher.cpp
@@ -0,0 +1,47 @@
+#include <memory>
+
+#include "fuzzy_impl.h"
+
+#include <fuzzy.h>
+
+FuzzyHasher::~FuzzyHasher() {
+  fuzzy_free(ctx);
+}
+
+FuzzyHasher::FuzzyHasher() :
+  ctx(fuzzy_new())
+{}
+
+FuzzyHasher::FuzzyHasher(const FuzzyHasher& other) :
+  ctx(fuzzy_clone(other.ctx))
+{}
+
+void FuzzyHasher::update(const uint8_t* beg, const uint8_t* end) {
+  if (!fuzzy_update(ctx, beg, end-beg)) {
+    // TODO: error!
+  }
+}
+
+void FuzzyHasher::set_total_input_length(uint64_t len) {
+  if (!fuzzy_set_total_input_length(ctx, len)) {
+    // TODO: error!
+  }
+}
+
+void FuzzyHasher::get(void* val) {
+  if (!fuzzy_digest(ctx, static_cast<char*>(val), 0)) {
+    // TODO: error!
+  }
+}
+void FuzzyHasher::reset() {
+  fuzzy_free(ctx);
+  ctx = fuzzy_new();
+}
+
+FuzzyHasher* FuzzyHasher::clone() const {
+  return new FuzzyHasher(*this);
+}
+
+std::unique_ptr<HasherImpl> make_fuzzy_hasher() {
+  return std::unique_ptr<FuzzyHasher>(new FuzzyHasher());
+}
--- src/fuzzy_matcher.cpp
+++ src/fuzzy_matcher.cpp
@@ -6,7 +6,6 @@
 
 #include <fuzzy.h>
 
-#include "fuzzy_impl.h"
 #include "fuzzy_matcher.h"
 #include "hasher.h"
 #include "parser.h"
@@ -289,46 +288,4 @@ std::unique_ptr<SFHASH_FuzzyMatcher> load_fuzzy_hashset(const char* beg, const c
     matcher->add(FuzzyHash{l->first, l->second});
   }
   return matcher;
-}
-
-FuzzyHasher::~FuzzyHasher() {
-  fuzzy_free(ctx);
-}
-
-FuzzyHasher::FuzzyHasher() :
-  ctx(fuzzy_new())
-{}
-
-FuzzyHasher::FuzzyHasher(const FuzzyHasher& other) :
-  ctx(fuzzy_clone(other.ctx))
-{}
-
-void FuzzyHasher::update(const uint8_t* beg, const uint8_t* end) {
-  if (!fuzzy_update(ctx, beg, end-beg)) {
-    // TODO: error!
-  }
-}
-
-void FuzzyHasher::set_total_input_length(uint64_t len) {
-  if (!fuzzy_set_total_input_length(ctx, len)) {
-    // TODO: error!
-  }
-}
-
-void FuzzyHasher::get(void* val) {
-  if (!fuzzy_digest(ctx, static_cast<char*>(val), 0)) {
-    // TODO: error!
-  }
-}
-void FuzzyHasher::reset() {
-  fuzzy_free(ctx);
-  ctx = fuzzy_new();
-}
-
-FuzzyHasher* FuzzyHasher::clone() const {
-  return new FuzzyHasher(*this);
-}
-
-std::unique_ptr<HasherImpl> make_fuzzy_hasher() {
-  return std::unique_ptr<FuzzyHasher>(new FuzzyHasher());
 }
