1cb2f7274e02114fc59488278016412298d68c6d
Joel Uckelman <juckelman@strozfriedberg.co.uk>
2018-08-22 15:25:26 +0100

    Added Jenkins build files.

---
+++ jenkins.sh
@@ -0,0 +1,38 @@
+#!/bin/bash -ex
+
+. $HOME/vendors/build_config.sh
+
+clean_it
+
+./bootstrap.sh
+
+build_it
+
+EXES="src/$EXE_DOT_LIBS/hasher$EXE_EXT \
+src/$EXE_DOT_LIBS/matcher$EXE_EXT"
+
+STAGE="$EXES python/hasher.py"
+
+case "$Target" in
+linux)
+  STAGE+=' src/.libs/libhasher.so*'
+  ;;
+
+windows)
+  case "$Linkage" in
+  shared)
+    DLL='src/.libs/libhasher.dll'
+    STAGE+=" $DLL $($VENDORS/gather.sh $DLL $EXES $MINGW_ROOT/bin $DEPS/bin | grep -v '/libhasher.dll$')"
+    ;;
+  shared-fat)
+    STAGE+=' src/.libs/libhasher.dll'
+    ;;
+  static)
+    check_static $EXES
+    STAGE+=' src/.libs/libhasher.a'
+    ;;
+  esac
+  ;;
+esac
+
+archive_it
