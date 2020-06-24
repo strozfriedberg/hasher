fb5706c8618f9a246c69d24662ecb61f13bc774e
Joel Uckelman <juckelman@strozfriedberg.co.uk>
2020-06-24 17:11:03 +0100

    Start of struct for holding both hashset info and data.

---
+++ include/hashsetholder.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "hasher/api.h"
+#include "hashset.h"
+
+#include <memory>
+
+struct SFHASH_HashSetHolder {
+  std::unique_ptr<SFHASH_HashSetInfo, void (*)(SFHASH_HashSetInfo*)> info;
+  std::unique_ptr<SFHASH_HashSet, void (*)(SFHASH_HashSet*)> hset;
+};
