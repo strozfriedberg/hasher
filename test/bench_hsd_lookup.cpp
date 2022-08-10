27f0a04da35a4e802ae451d3fd6d48c2c3c32087
Joel Uckelman <juckelman@strozfriedberg.co.uk>
2022-08-10 16:30:49 +0100

    t - Added infrastructure for hashset data lookup agreement tests.

--- test/bench_hsd_lookup.cpp
+++ test/bench_hsd_lookup.cpp
@@ -1,16 +1,24 @@
 #include <catch2/catch_test_macros.hpp>
 #include <catch2/benchmark/catch_benchmark.hpp>
 
+#include <algorithm>
 #include <array>
 #include <filesystem>
 #include <fstream>
+#include <iterator>
+#include <memory>
+#include <ostream>
 #include <random>
 #include <string>
+#include <sstream>
 #include <vector>
 
 #include "hasher/api.h"
 
 #include "hashsetdata.h"
+#include "hashsetinfo.h"
+#include "hex.h"
+#include "throw.h"
 #include "util.h"
 
 const std::filesystem::path VS{"test/virusshare-389.hset"};
@@ -92,6 +100,24 @@ auto make_random_hashes(RNG& rng, size_t count) {
   return hashes;
 }
 
+// sample some hashes from hashset data
+template <
+  size_t HashLength,
+  class Generator,
+  class Holder
+>
+auto sample_hashes(Generator& gen, const Holder& h, size_t count) {
+  std::vector<std::array<uint8_t, HashLength>> hashes;
+  std::sample(
+    static_cast<const std::array<uint8_t, HashLength>*>(h.beg),
+    static_cast<const std::array<uint8_t, HashLength>*>(h.end),
+    std::back_inserter(hashes),
+    count,
+    gen
+  );
+  return hashes;
+}
+
 // make a HashSetDataImpl from hashset data 
 template <
   size_t HashLength,
@@ -164,3 +190,114 @@ void do_some_lookups(HashGenerator& gen, const SetList& sets, size_t min, size_t
   }
 }
 
+template <
+  size_t HashLength,
+  class Holder,
+  class SetList
+>
+void do_benchmark(const Holder& h, const SetList& hsds) {
+  RNG rng;
+  auto gen = [&rng](size_t count) {
+    return make_random_hashes<HashLength>(rng, count);
+  };
+
+  do_some_lookups(gen, hsds, 1, 100000, 10);
+}
+
+// check that all results for the given hash match
+template <
+  class Hash
+>
+bool check_agreement(const Hash& h, const std::vector<bool>& hits, std::ostream& out) {
+
+  const bool ok = std::find(hits.begin() + 1, hits.end(), !hits[0]) == hits.end();
+/*
+  if (!ok) {
+    std::ostringstream ss;
+    ss << to_hex(h) << ": ";
+    for (auto b: hits) {
+      ss << b;
+    }
+    FAIL(ss.str());
+  }
+*/
+
+  out << to_hex(h) << ": ";
+  for (auto b: hits) {
+    out << b;
+  }
+  out << '\n';
+
+  return ok;
+}
+
+template <
+  class LookupList,
+  class SetList
+>
+bool check_agreement(const LookupList& lookups, const SetList& hsds, std::ostream& out) {
+  bool ok = true;
+  std::vector<bool> hits(hsds.size());
+  for (const auto& h: lookups) {
+    for (size_t i = 0; i < hsds.size(); ++i) {
+      hits[i] = hsds[i].second->contains(h.data());
+    }
+    ok &= check_agreement(h, hits, out);
+  }
+  return ok;
+}
+
+template <
+  size_t HashLength,
+  class Holder,
+  class SetList
+>
+void do_agreement_check(const Holder& h, const SetList& hsds) {
+  // lookup some random hashes
+  RNG rng;
+  auto gen = [&rng](size_t count) {
+    return make_random_hashes<HashLength>(rng, count);
+  };
+
+  bool ok = true;
+  std::ostringstream ss;
+
+  const auto lookup_random = gen(1000);
+  ok &= check_agreement(lookup_random, hsds, ss);
+
+  // lookup some randomly sampled hashes
+  const auto lookup_sample = sample_hashes<HashLength>(rng.re, h, 1000);
+  ok &= check_agreement(lookup_sample, hsds, ss);
+
+  if (!ok) {
+    FAIL(ss.str());
+  }
+}
+
+template <
+  size_t HashLength,
+  class Holder
+>
+auto make_hsds(const Holder& h) {
+  const auto hsinfo = load_header(h.beg, h.end);
+  REQUIRE(hsinfo->hash_length == HashLength);
+
+  std::vector<std::pair<std::string, std::unique_ptr<HashSetData>>> hsds;
+// TODO: why can't these go into an intializer list?
+  hsds.emplace_back("std", make_std_hsd<HashLength>(h, *hsinfo));
+  hsds.emplace_back("radius", make_radius_hsd<HashLength>(h, *hsinfo));
+
+  return hsds;
+}
+
+TEST_CASE("MemoryLookupCheckVirusShare") {
+  MemoryHolder h{read_file(VS)};
+  const auto hsds = make_hsds<VS_HLEN>(h);
+  do_agreement_check<VS_HLEN>(h, hsds);
+}
+
+TEST_CASE("MemoryLookupCheckNSRL") {
+  MemoryHolder h{read_file(NSRL)};
+  const auto hsds = make_hsds<NSRL_HLEN>(h);
+  do_agreement_check<NSRL_HLEN>(h, hsds);
+}
