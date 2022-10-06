7d431bf65ea5f9d33fae5dff034f131679efede4
Joel Uckelman <juckelman@strozfriedberg.co.uk>
2022-10-06 14:32:42 +0100

    F - api.h now acts as master header, including all others.

--- include/hasher/api.h
+++ include/hasher/api.h
@@ -2,6 +2,10 @@
 #define HASHER_C_API_H_
 
 #include <hasher/common.h>
+#include <hasher/fuzzy.h>
+#include <hasher/hasher.h>
+#include <hasher/hashset.h>
+#include <hasher/hex.h>
 
 #ifdef __cplusplus
 extern "C" {
