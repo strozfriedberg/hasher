737308c605fdb54164939aa9271fe6ee6ca01906
Joel Uckelman <juckelman@strozfriedberg.co.uk>
2022-10-27 12:41:54 +0100

    t - Removed cruft.

--- test/test_hset_encoder.cpp
+++ test/test_hset_encoder.cpp
@@ -42,51 +42,6 @@ TEST_CASE("size_to_u64_bad") {
   }
 }
 
-ssize_t write_vec(void* ctx, const void* buf, size_t len) {
-  static_cast<std::vector<char>*>(ctx)->insert(
-    static_cast<std::vector<char>*>(ctx)->end(),
-    static_cast<const char*>(buf),
-    static_cast<const char*>(buf) + len
-  );
-  return static_cast<ssize_t>(len);
-}
-
-TEST_CASE("write_chunk_nonempty") {
-  const char type[] = "ABCD";
-  const char data[] = "0123456789";
-
-  REQUIRE(std::strlen(data) == 10);
-
-  // echo -n 0123456789 | sha256sum
-  const auto hash = to_bytes<32>("84d89877f0d4041efb6bf91a16f0248f2fd573e6af05c19f96bedb9f882f7882");
-
-  REQUIRE(hash.size() == 32);
-
-  std::vector<char> out;
-  Writer w{write_vec, &out};
-
-  const uint64_t exp_size = 4 + 8 + 10 + 32;
-
-  // Output size
-  CHECK(write_chunk(type, data, 10, w) == exp_size);
-  CHECK(out.size() == exp_size);
-
-  // Output value
-  auto c = out.cbegin();
-  CHECK(std::equal(c, c + 4, type));
-  c += 4;
-
-  const auto dlen = { 0x0A, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
-  CHECK(std::equal(c, c + 8, dlen.begin()));
-  c += 8;
-
-  CHECK(std::equal(c, c + 10, data));
-  c += 10;
-
-  CHECK(std::equal(c, c + hash.size(), reinterpret_cast<const char*>(hash.data())));
-}
-*/
-
 TEST_CASE("length_alignment_padding") {
   const std::initializer_list<std::tuple<uint64_t, uint64_t, size_t>> tests = {
     {    0, 4096,    0 },
