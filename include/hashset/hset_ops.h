f6e8476849ddf316ad1b9fb2487f207b41d5898b
Joel Uckelman <juckelman@strozfriedberg.co.uk>
2022-11-04 17:59:32 +0000

    R - Refactored set ops for easier testability.

--- include/hashset/hset_ops.h
+++ include/hashset/hset_ops.h
@@ -5,11 +5,7 @@
 #include "throw.h"
 #include "hasher/hashset.h"
 
-#include <algorithm>
-#include <cstring>
 #include <cstddef>
-#include <iterator>
-#include <set>
 #include <vector>
 #include <utility>
 
@@ -38,88 +34,32 @@ void make_output_records(const uint8_t* ibeg, const uint8_t* iend, size_t irlen,
   }
 }
 
-template <auto op>
 std::pair<
   std::vector<std::vector<uint8_t>>,
   std::vector<SFHASH_HashAlgorithm>
-> apply_op(
+> union_op(
   const RecordHeader& lrhdr,
   const RecordData& lrdat,
   const RecordHeader& rrhdr,
-  const RecordData& rrdat)
-{
-  // collect the hash types from the input hashsets
-  std::set<std::pair<SFHASH_HashAlgorithm, size_t>> ltypes, rtypes;
+  const RecordData& rrdat
+);
 
-  collect_hash_types(lrhdr.fields, ltypes);
-  collect_hash_types(rrhdr.fields, rtypes);
+std::pair<
+  std::vector<std::vector<uint8_t>>,
+  std::vector<SFHASH_HashAlgorithm>
+> intersect_op(
+  const RecordHeader& lrhdr,
+  const RecordData& lrdat,
+  const RecordHeader& rrhdr,
+  const RecordData& rrdat
+);
 
-  THROW_IF(
-    ltypes != rtypes,
-    "hash type mismatch between left and right operands"
-  );
-
-  std::vector<std::pair<SFHASH_HashAlgorithm, size_t>> otypes;
-
-  // output types is the union of the left and right input types
-  std::set_union(
-    ltypes.begin(), ltypes.end(),
-    rtypes.begin(), rtypes.end(),
-    std::back_inserter(otypes)
-  );
-
-  // create mapping from output fields to input fields
-  std::vector<std::pair<int, size_t>> lf, rf;
-  for (const auto& [a, hlen]: otypes) {
-    lf.emplace_back(hashset_record_field_index_for_type(lrhdr, a), hlen);
-    rf.emplace_back(hashset_record_field_index_for_type(rrhdr, a), hlen);
-  }
-
-  std::vector<std::vector<uint8_t>> l_ex, r_ex;
-
-  // rewrite left records to joint format
-  make_output_records(
-    static_cast<const uint8_t*>(lrdat.beg),
-    static_cast<const uint8_t*>(lrdat.end),
-    lrhdr.record_length,
-    lf,
-    l_ex
-  );
-
-  // rewrite right records to joint format
-  make_output_records(
-    static_cast<const uint8_t*>(rrdat.beg),
-    static_cast<const uint8_t*>(rrdat.end),
-    rrhdr.record_length,
-    rf,
-    r_ex
-  );
-
-  // ensure that the records to be unioned are sorted and unique
-  std::sort(l_ex.begin(), l_ex.end());
-  l_ex.erase(std::unique(l_ex.begin(), l_ex.end()), l_ex.end());
-
-  std::sort(r_ex.begin(), r_ex.end());
-  r_ex.erase(std::unique(r_ex.begin(), r_ex.end()), r_ex.end());
-
-  // apply the op to the records
-  std::vector<std::vector<uint8_t>> u;
-
-  op(
-    l_ex.begin(), l_ex.end(),
-    r_ex.begin(), r_ex.end(),
-    std::back_inserter(u)
-  );
-
-  // make the list of output types
-  std::vector<SFHASH_HashAlgorithm> htypes;
-  std::transform(
-    otypes.begin(), otypes.end(),
-    std::back_inserter(htypes),
-    [](const auto& h) {
-      return h.first;
-    }
-  );
-
-  return { std::move(u), std::move(htypes) };
-}
+std::pair<
+  std::vector<std::vector<uint8_t>>,
+  std::vector<SFHASH_HashAlgorithm>
+> difference_op(
+  const RecordHeader& lrhdr,
+  const RecordData& lrdat,
+  const RecordHeader& rrhdr,
+  const RecordData& rrdat
+);
--- src/lib/hashset/hset_ops.cpp
+++ src/lib/hashset/hset_ops.cpp
@@ -4,6 +4,96 @@
 #include "hashset/hset.h"
 #include "hashset/hset_ops.h"
 
+#include <algorithm>
+#include <iterator>
+#include <set>
+
+template <auto op>
+std::pair<
+  std::vector<std::vector<uint8_t>>,
+  std::vector<SFHASH_HashAlgorithm>
+> apply_op(
+  const RecordHeader& lrhdr,
+  const RecordData& lrdat,
+  const RecordHeader& rrhdr,
+  const RecordData& rrdat)
+{
+  // collect the hash types from the input hashsets
+  std::set<std::pair<SFHASH_HashAlgorithm, size_t>> ltypes, rtypes;
+
+  collect_hash_types(lrhdr.fields, ltypes);
+  collect_hash_types(rrhdr.fields, rtypes);
+
+  THROW_IF(
+    ltypes != rtypes,
+    "hash type mismatch between left and right operands"
+  );
+
+  std::vector<std::pair<SFHASH_HashAlgorithm, size_t>> otypes;
+
+  // output types is the union of the left and right input types
+  std::set_union(
+    ltypes.begin(), ltypes.end(),
+    rtypes.begin(), rtypes.end(),
+    std::back_inserter(otypes)
+  );
+
+  // create mapping from output fields to input fields
+  std::vector<std::pair<int, size_t>> lf, rf;
+  for (const auto& [a, hlen]: otypes) {
+    lf.emplace_back(hashset_record_field_index_for_type(lrhdr, a), hlen);
+    rf.emplace_back(hashset_record_field_index_for_type(rrhdr, a), hlen);
+  }
+
+  std::vector<std::vector<uint8_t>> l_ex, r_ex;
+
+  // rewrite left records to joint format
+  make_output_records(
+    static_cast<const uint8_t*>(lrdat.beg),
+    static_cast<const uint8_t*>(lrdat.end),
+    lrhdr.record_length,
+    lf,
+    l_ex
+  );
+
+  // rewrite right records to joint format
+  make_output_records(
+    static_cast<const uint8_t*>(rrdat.beg),
+    static_cast<const uint8_t*>(rrdat.end),
+    rrhdr.record_length,
+    rf,
+    r_ex
+  );
+
+  // ensure that the records to be unioned are sorted and unique
+  std::sort(l_ex.begin(), l_ex.end());
+  l_ex.erase(std::unique(l_ex.begin(), l_ex.end()), l_ex.end());
+
+  std::sort(r_ex.begin(), r_ex.end());
+  r_ex.erase(std::unique(r_ex.begin(), r_ex.end()), r_ex.end());
+
+  // apply the op to the records
+  std::vector<std::vector<uint8_t>> u;
+
+  op(
+    l_ex.begin(), l_ex.end(),
+    r_ex.begin(), r_ex.end(),
+    std::back_inserter(u)
+  );
+
+  // make the list of output types
+  std::vector<SFHASH_HashAlgorithm> htypes;
+  std::transform(
+    otypes.begin(), otypes.end(),
+    std::back_inserter(htypes),
+    [](const auto& h) {
+      return h.first;
+    }
+  );
+
+  return { std::move(u), std::move(htypes) };
+}
+
 template <auto op>
 SFHASH_HashsetBuildCtx* setop_open(
   const SFHASH_Hashset* l,
@@ -13,7 +103,7 @@ SFHASH_HashsetBuildCtx* setop_open(
   SFHASH_Error** err)
 {
   try {
-    const auto [u, htypes] = apply_op<op>(
+    const auto [u, htypes] = op(
       l->holder.rhdr,
       l->holder.rdat,
       r->holder.rhdr,
@@ -47,6 +137,75 @@ SFHASH_HashsetBuildCtx* setop_open(
   }
 }
 
+std::pair<
+  std::vector<std::vector<uint8_t>>,
+  std::vector<SFHASH_HashAlgorithm>
+> union_op(
+  const RecordHeader& lrhdr,
+  const RecordData& lrdat,
+  const RecordHeader& rrhdr,
+  const RecordData& rrdat)
+{
+  return apply_op<
+    std::set_union<
+      std::vector<std::vector<uint8_t>>::const_iterator,
+      std::vector<std::vector<uint8_t>>::const_iterator,
+      std::back_insert_iterator<std::vector<std::vector<uint8_t>>>
+    >
+  >(
+    lrhdr,
+    lrdat,
+    rrhdr,
+    rrdat
+  );
+}
+
+std::pair<
+  std::vector<std::vector<uint8_t>>,
+  std::vector<SFHASH_HashAlgorithm>
+> intersect_op(
+  const RecordHeader& lrhdr,
+  const RecordData& lrdat,
+  const RecordHeader& rrhdr,
+  const RecordData& rrdat)
+{
+  return apply_op<
+    std::set_intersection<
+      std::vector<std::vector<uint8_t>>::const_iterator,
+      std::vector<std::vector<uint8_t>>::const_iterator,
+      std::back_insert_iterator<std::vector<std::vector<uint8_t>>>
+    >
+  >(
+    lrhdr,
+    lrdat,
+    rrhdr,
+    rrdat
+  );
+}
+
+std::pair<
+  std::vector<std::vector<uint8_t>>,
+  std::vector<SFHASH_HashAlgorithm>
+> difference_op(
+  const RecordHeader& lrhdr,
+  const RecordData& lrdat,
+  const RecordHeader& rrhdr,
+  const RecordData& rrdat)
+{
+  return apply_op<
+    std::set_difference<
+      std::vector<std::vector<uint8_t>>::const_iterator,
+      std::vector<std::vector<uint8_t>>::const_iterator,
+      std::back_insert_iterator<std::vector<std::vector<uint8_t>>>
+    >
+  >(
+    lrhdr,
+    lrdat,
+    rrhdr,
+    rrdat
+  );
+}
+
 SFHASH_HashsetBuildCtx* sfhash_union_hashsets_open(
   const SFHASH_Hashset* l,
   const SFHASH_Hashset* r,
@@ -54,13 +213,9 @@ SFHASH_HashsetBuildCtx* sfhash_union_hashsets_open(
   const char* result_hashset_desc,
   SFHASH_Error** err)
 {
-  return setop_open<
-    std::set_union<
-      std::vector<std::vector<uint8_t>>::const_iterator,
-      std::vector<std::vector<uint8_t>>::const_iterator,
-      std::back_insert_iterator<std::vector<std::vector<uint8_t>>>
-    >
-  >(l, r, result_hashset_name, result_hashset_desc, err);
+  return setop_open<union_op>(
+    l, r, result_hashset_name, result_hashset_desc, err
+  );
 }
 
 SFHASH_HashsetBuildCtx* sfhash_intersect_hashsets_open(
@@ -70,13 +225,9 @@ SFHASH_HashsetBuildCtx* sfhash_intersect_hashsets_open(
   const char* result_hashset_desc,
   SFHASH_Error** err)
 {
-  return setop_open<
-    std::set_intersection<
-      std::vector<std::vector<uint8_t>>::const_iterator,
-      std::vector<std::vector<uint8_t>>::const_iterator,
-      std::back_insert_iterator<std::vector<std::vector<uint8_t>>>
-    >
-  >(l, r, result_hashset_name, result_hashset_desc, err);
+  return setop_open<intersect_op>(
+    l, r, result_hashset_name, result_hashset_desc, err
+  );
 }
 
 SFHASH_HashsetBuildCtx* sfhash_subtract_hashsets_open(
@@ -86,11 +237,7 @@ SFHASH_HashsetBuildCtx* sfhash_subtract_hashsets_open(
   const char* result_hashset_desc,
   SFHASH_Error** err)
 {
-  return setop_open<
-    std::set_difference<
-      std::vector<std::vector<uint8_t>>::const_iterator,
-      std::vector<std::vector<uint8_t>>::const_iterator,
-      std::back_insert_iterator<std::vector<std::vector<uint8_t>>>
-    >
-  >(l, r, result_hashset_name, result_hashset_desc, err);
+  return setop_open<difference_op>(
+    l, r, result_hashset_name, result_hashset_desc, err
+  );
 }
