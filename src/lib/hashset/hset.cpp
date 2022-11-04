d57508634998db89d7aebadd50ee5175825bf8e0
Joel Uckelman <juckelman@strozfriedberg.co.uk>
2022-11-04 14:50:11 +0000

    F - Implemented sfhash_hashset_record_field_index_for_type, =sfhash_hashset_record_field, sfhash_hashset_record_for_hash; added stub for sfhash_hashset_records_lookup. Added tests.

--- src/lib/hashset/hset.cpp
+++ src/lib/hashset/hset.cpp
@@ -6,6 +6,7 @@
 
 #include <algorithm>
 #include <memory>
+#include <numeric>
 
 SFHASH_Hashset* sfhash_load_hashset(
   const void* beg,
@@ -51,4 +52,54 @@ bool sfhash_hashset_lookup(
   const void* hash
 ) {
   return std::get<std::unique_ptr<LookupStrategy>>(hset->holder.hsets[tidx])->contains(static_cast<const uint8_t*>(hash));
+}
+
+int sfhash_hashset_record_field_index_for_type(
+  const SFHASH_Hashset* hset,
+  SFHASH_HashAlgorithm htype
+) {
+
+  const auto i = std::find_if(
+    hset->holder.hsets.begin(),
+    hset->holder.hsets.end(),
+    [htype](const auto& t) {
+      return htype == std::get<HashsetHeader>(t).hash_type;
+    }
+  );
+
+// TODO: consider storing this rather than computing it each time
+  return i == hset->holder.hsets.end() ? -1 :
+    std::accumulate(
+      hset->holder.hsets.begin(), i, 0,
+      [](int off, const auto& h) {
+        return off + 1 + std::get<HashsetHeader>(h).hash_length;
+      }
+    );
+}
+
+const void* sfhash_hashset_record_field(
+  const SFHASH_HashsetRecord* rec,
+  size_t tidx
+) {
+  return reinterpret_cast<const uint8_t*>(rec) + tidx;
+}
+
+const SFHASH_HashsetRecordRange sfhash_hashset_records_lookup(
+  const SFHASH_Hashset* hset,
+  size_t tidx,
+  const void* hash
+) {
+// TODO: std::binary_search doesn't return a location, so we have nothing
+// to use yet to implement this; maybe we need std::lower_bound?
+  return {};
+}
+
+const SFHASH_HashsetRecord* sfhash_hashset_record_for_hash(
+  const SFHASH_Hashset* hset,
+  size_t tidx,
+  size_t ridx
+) {
+  const auto& ri = std::get<RecordIndex>(hset->holder.hsets[tidx]);
+  const auto record_index = static_cast<const uint64_t*>(ri.beg)[ridx];
+  return reinterpret_cast<const SFHASH_HashsetRecord*>(static_cast<const uint8_t*>(hset->holder.rdat.beg) + record_index * hset->holder.rhdr.record_length);
 }
--- test/test_hashset_api.cpp
+++ test/test_hashset_api.cpp
@@ -179,8 +179,55 @@ TEST_CASE("hashset_lookup") {
 // TODO: lookup with bad hset ptr?
 // TODO: lookup with bad hash type index?
 
-/*
-constexpr std::initializer_list<SFHASH_HashsetType> HASH_TYPES = {
+TEST_CASE("hashset_record_field_index_for_type") {
+  SFHASH_Hashset hset;
+
+  hset.holder.hsets.emplace_back(
+    HashsetHeader{ SFHASH_SIZE, "size", 8, 0 },
+    HashsetHint{},
+    HashsetData{},
+    std::unique_ptr<LookupStrategy>(),
+    RecordIndex{}
+  );
+
+  hset.holder.hsets.emplace_back(
+    HashsetHeader{ SFHASH_MD5, "MD5", 16, 0 },
+    HashsetHint{},
+    HashsetData{},
+    std::unique_ptr<LookupStrategy>(),
+    RecordIndex{}
+  );
+
+  hset.holder.hsets.emplace_back(
+    HashsetHeader{ SFHASH_SHA_1, "SHA-1", 20, 0 },
+    HashsetHint{},
+    HashsetData{},
+    std::unique_ptr<LookupStrategy>(),
+    RecordIndex{}
+  );
+
+  CHECK(sfhash_hashset_record_field_index_for_type(&hset, SFHASH_SIZE) == 0);
+  CHECK(sfhash_hashset_record_field_index_for_type(&hset, SFHASH_MD5) == 9);
+  CHECK(sfhash_hashset_record_field_index_for_type(&hset, SFHASH_SHA_1) == 26);
+}
+
+TEST_CASE("hashset_record_field") {
+  uint8_t rec[1 + 16 + 1 + 20 + 1 + 8];
+  rec[0] = 1;
+  from_hex(rec + 1, "081d3d40b257d8bbc5858345ad186beb", 16);
+  rec[17] = 1;
+  from_hex(rec + 18, "fad37e52be19b7a6ea321b848f2d6de4b75efcc9", 20);
+  rec[38] = 1;
+  *reinterpret_cast<uint64_t*>(&rec[39]) = 42;
+
+  const SFHASH_HashsetRecord* r = reinterpret_cast<const SFHASH_HashsetRecord*>(&rec);
+
+  CHECK(!std::memcmp(sfhash_hashset_record_field(r, 0), &rec[0], 17));
+  CHECK(!std::memcmp(sfhash_hashset_record_field(r, 17), &rec[17], 21));
+  CHECK(!std::memcmp(sfhash_hashset_record_field(r, 38), &rec[38], 9));
+}
+
+constexpr std::initializer_list<SFHASH_HashAlgorithm> HASH_TYPES = {
   SFHASH_SIZE,
   SFHASH_MD5,
   SFHASH_SHA_1,
@@ -214,58 +261,148 @@ constexpr size_t HASH_LENGTH[] = {
   8
 };
 
+// There's no obvious way presently to deal with missing record values
+// Do we care about this? We'd need to add a byte to each field to indicate
+// whether the field is populated.
+
 template <class Tests>
 void do_record_lookups(
   const SFHASH_Hashset* hset,
-  SFHASH_HashsetType htype,
+  SFHASH_HashAlgorithm htype,
   const Tests& tests
 ) {
   const auto tidx = sfhash_hashset_index_for_type(hset, htype);
   REQUIRE(tidx != -1);
 
-  for (const auto [hash, exp]: tests) {
-    for (auto [i, end] = sfhash_hashset_records_lookup(hset, tidx, hash); i != end; ++i) {
-      const SFHASH_HashsetRecord* r = sfhash_hashset_record_for_hash(hset, tidx, i);
-      for (auto t: HASH_TYPES) {
-        const void* f = sfhash_hashset_record_field(r, t);
+  // list out all the hash types in the hashset 
+  std::vector<std::pair<SFHASH_HashAlgorithm, int>> rec_offs;
+  for (auto t: HASH_TYPES) {
+    const auto toff = sfhash_hashset_record_field_index_for_type(hset, t);
+    if (toff != -1) {
+      rec_offs.emplace_back(t, toff);
+    }
+  }
 
-// HERE
-        if (std::get<t>exp[t]) {
-          CHECK(std::memcmp(exp[t], f, HASH_LENGTH[t]) == 0);
-        }
-        else {
-          // no expected value, should both be null
-          CHECK(!f);
-        }
+  for (const auto& [hash, exp]: tests) {
+    for (auto [i, end] = sfhash_hashset_records_lookup(hset, tidx, hash.data()); i != end; ++i) {
+      const auto r = sfhash_hashset_record_for_hash(hset, tidx, i);
+      for (const auto& [t, toff]: rec_offs) {
+        const auto f = sfhash_hashset_record_field(r, toff);
+        const auto e = exp.find(t);
+        CHECK((e != exp.end() && !std::memcmp(&*e, f, HASH_LENGTH[t])));
       }
     }
   }
 }
 
 TEST_CASE("hashset_record_lookup") {
-  const auto f = read_file("test/test.hset");
-  const auto beg = f.data();
-  const auto end = beg + f.size();
+  // create a test hset
+  const std::array md5s{
+    to_bytes<16>("081d3d40b257d8bbc5858345ad186beb"),
+    to_bytes<16>("19875d9651a319fdb8f7332b660c432d"),
+    to_bytes<16>("297da3f7f9c7a38fcb8872193cf3b609"),
+    to_bytes<16>("56f8af1ba509b13c01a0eb64c015bcd9"),
+    to_bytes<16>("6a9ffae1487ee7c347b188ecfcdf1bd3"),
+    to_bytes<16>("7932bda72aa49ba324e51a5410fd569f"),
+    to_bytes<16>("857cc4681e7a952cbd26042140f5a2cc"),
+    to_bytes<16>("a4b262726db358a71d80ced2cc871b45"),
+    to_bytes<16>("abdba04b7527c095bb79e19db8e1de13"),
+    to_bytes<16>("eb6fe7367307473c86c3438744c3b1db")
+  };
 
-  SFHASH_Error* err = nullptr;
+  const std::array sha1s{
+    to_bytes<20>("0af8e028c7048ad772ddec2200ec7e0e4d58b0c3"),
+    to_bytes<20>("1127ceb2c2d789c1d7615b12082ca30222f3c612"),
+    to_bytes<20>("3e909896b309492e00444212bb2b270b5809a0cf"),
+    to_bytes<20>("5cb3a026273fd180a9cfc32bfe3da8730e4bd192"),
+    to_bytes<20>("6007cca8643961ed5f374d2dbf0394dc88110cdb"),
+    to_bytes<20>("78e7f1736d31d8b5b1beb41e2e41769754d3cf3f"),
+    to_bytes<20>("a40ea2ba2d45f9aa4d2b31adfcad0bbdb6670452"),
+    to_bytes<20>("b46c74716a8b1fbf5fedc75f58c9c72b53631123"),
+    to_bytes<20>("c1ec34d963283b8ca0ac899164dfbb3fc2321e38"),
+    to_bytes<20>("fad37e52be19b7a6ea321b848f2d6de4b75efcc9")
+  };
 
-  auto hset = make_unique_del(
-    sfhash_load_hashset(beg, end, &err),
-    sfhash_destroy_hashset
+  const std::array<uint64_t, 10> sizes{
+    0,
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9
+  };
+
+  std::array<
+    std::tuple<
+      std::array<uint8_t, 16>,
+      std::array<uint8_t, 20>,
+      uint64_t
+    >, 10
+  > recs;  
+     
+  std::array<uint64_t, 10> md5s_r, sha1s_r, sizes_r;
+
+  for (int i = 0; i < 10; ++i) {
+    recs[i] = { md5s[i], sha1s[9-i], sizes[i] };
+    md5s_r[i] = i;
+    sha1s_r[i] = 9-i;
+    sizes_r[i] = i;
+  }
+
+  SFHASH_Hashset hset;
+
+  hset.holder.rhdr = {
+    44,
+    10,
+    {
+      { SFHASH_MD5,   "MD5",   16 },
+      { SFHASH_SHA_1, "SHA-1", 20 },
+      { SFHASH_SIZE,  "size",   8 }
+    }
+  };
+
+  hset.holder.rdat = { recs.begin(), recs.end() };
+
+  hset.holder.hsets.emplace_back(
+    HashsetHeader{ SFHASH_MD5, "MD5", 16, 0 },
+    HashsetHint{},
+    HashsetData{ md5s.begin(), md5s.end() },
+    std::unique_ptr<LookupStrategy>(),
+    RecordIndex{ md5s_r.begin(), md5s_r.end() }
   );
 
-  REQUIRE(!err);
-  REQUIRE(hset);
+  hset.holder.hsets.emplace_back(
+    HashsetHeader{ SFHASH_SHA_1, "SHA-1", 20, 0 },
+    HashsetHint{},
+    HashsetData{ sha1s.begin(), sha1s.end() },
+    std::unique_ptr<LookupStrategy>(),
+    RecordIndex{ sha1s_r.begin(), sha1s_r.end() }
+  );
+
+  hset.holder.hsets.emplace_back(
+    HashsetHeader{ SFHASH_SIZE, "size", 8, 0 },
+    HashsetHint{},
+    HashsetData{ sizes.begin(), sizes.end() },
+    std::unique_ptr<LookupStrategy>(),
+    RecordIndex{ sizes_r.begin(), sizes_r.end() }
+  );
 
   // lookup records for some MD5s
 
-  const std::vector<
+  std::vector<
     std::pair<
-      std::array<uint8_t,16>,
-      std::tuple<
-        std::optional<uint64_t>,
-        std::optional<std::array<uint8_t,16>>,
-        std::optional<std::array<uint8_t,20>>
+      std::array<uint8_t, 16>,
+      std::map<
+        SFHASH_HashAlgorithm,
+        std::variant<
+          std::array<uint8_t, 16>,
+          std::array<uint8_t, 20>,
+          uint64_t
+        >
       >
     >
   > tests_16{
@@ -283,7 +420,73 @@ TEST_CASE("hashset_record_lookup") {
     }
   };
 
-  do_record_lookups(hset.get(), SFHASH_MD5, tests_16);
+  for (int i = 0; i < 10; ++i) {
+    tests_16.emplace_back(
+      md5s[i], 
+      std::map<SFHASH_HashAlgorithm,
+        std::variant<
+          std::array<uint8_t, 16>,
+          std::array<uint8_t, 20>,
+          uint64_t
+        >
+      >{
+        { SFHASH_MD5, md5s[i] },
+        { SFHASH_SHA_1, sha1s[9-i] },
+        { SFHASH_SIZE, sizes[i] }
+      }
+    );
+  }
 
+  do_record_lookups(&hset, SFHASH_MD5, tests_16);
+
+  // lookup records for some SHA1s
+
+  std::vector<
+    std::pair<
+      std::array<uint8_t, 20>,
+      std::map<
+        SFHASH_HashAlgorithm,
+        std::variant<
+          std::array<uint8_t, 16>,
+          std::array<uint8_t, 20>,
+          uint64_t
+        >
+      >
+    >
+  > tests_20{
+    {
+      to_bytes<20>("0000000000000000000000000000000000000000"),
+      {}
+    },
+    {
+      to_bytes<20>("deadbeefdeadbeefdeadbeefdeadbeefdeadbeef"),
+      {}
+    },
+    {
+      to_bytes<20>("ffffffffffffffffffffffffffffffffffffffff"),
+      {}
+    }
+  };
+
+  for (int i = 0; i < 10; ++i) {
+    tests_20.emplace_back(
+      sha1s[i], 
+      std::map<SFHASH_HashAlgorithm,
+        std::variant<
+          std::array<uint8_t, 16>,
+          std::array<uint8_t, 20>,
+          uint64_t
+        >
+      >{
+        { SFHASH_MD5, md5s[9-i] },
+        { SFHASH_SHA_1, sha1s[i] },
+        { SFHASH_SIZE, sizes[9-i] }
+      }
+    );
+  }
+
+  do_record_lookups(&hset, SFHASH_SHA_1, tests_20);
 }
-*/
+
+// { 1, "test", "2022-10-05T12:05:35Z", "a test" }
+
