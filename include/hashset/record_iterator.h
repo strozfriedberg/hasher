d6e90fcd2e2f9e792d1de8c5f481a52e2f316075
Joel Uckelman <juckelman@strozfriedberg.co.uk>
2023-02-07 17:14:36 +0000

    B - Some C++ std libs want more comparison operators for std algs.

--- include/hashset/record_iterator.h
+++ include/hashset/record_iterator.h
@@ -189,6 +189,10 @@ public:
     return cur > o.cur;
   }
 
+  bool operator>=(const RecordIterator& o) const noexcept {
+    return cur >= o.cur;
+  }
+
 // C++20:
 /*
   auto operator<=>(const RecordIterator& o) const noexcept {
@@ -374,8 +378,16 @@ public:
     return pos < o.pos;
   }
 
+  bool operator<=(const HashRecordIterator& o) const noexcept {
+    return pos <= o.pos;
+  }
+
   bool operator>(const HashRecordIterator& o) const noexcept {
     return pos > o.pos;
+  }
+
+  bool operator>=(const HashRecordIterator& o) const noexcept {
+    return pos >= o.pos;
   }
 
 // C++20:
