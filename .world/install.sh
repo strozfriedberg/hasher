c75f18baa399fd7b8bd6ac11b1bca98e5b260d05
Joel Uckelman <juckelman@strozfriedberg.co.uk>
2024-01-29 14:37:51 +0000

    F - Build a Linux wheel.

--- .world/install.sh
+++ .world/install.sh
@@ -5,4 +5,5 @@
 install_it
 
 mkdir -p $INSTALL/lib/python
-cp python/hasher.py utils/{hsdump.py,hsinfo.py,mkhashset.py,nsrldump.py} $INSTALL/lib/python
+cp -av python $INSTALL/lib/python/hasher
+cp utils/{hsdump.py,hsinfo.py,mkhashset.py,nsrldump.py} $INSTALL/lib/python
