fe4451446c59c1f52cbae99e6729dddc1f461aab
Joel Uckelman <juckelman@strozfriedberg.co.uk>
2024-01-30 12:54:17 +0000

    B - Install module to the right directory.

--- .world/install.sh
+++ .world/install.sh
@@ -5,5 +5,5 @@
 install_it
 
 mkdir -p $INSTALL/lib/python
-cp -av python $INSTALL/lib/python/hasher
+cp -av python/hasher $INSTALL/lib/python
 cp utils/{hsdump.py,hsinfo.py,mkhashset.py,nsrldump.py} $INSTALL/lib/python
